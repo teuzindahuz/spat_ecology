375e45ae263323160ecb2f43aadaea182b96ce41
teuzindahuz <m.lattuada87@gmail.com>
2020-03-18 22:48:08 +0100

    Update ukrbin.R

--- ukrbin.R
+++ ukrbin.R
@@ -1,22 +1,26 @@
-## automatize species download from ukrbin
+#### automatize species download from ukrbin
 ## libraries
 library(stringr)
-#read page, example with amphibians
-amp.page<-readLines('http://www.ukrbin.com/distribution.php?action=geotaxa&geoid=182&classid=51442&sp=1')
-# extract html lines with species names and id
+##read page, example with amphibians
+amp.page<-readLines('http://www.ukrbin.com/distribution.php?action=geotaxa&geoid=182&classid=51442&sp=1') #the http is the 
+#page of the species of interest
+
+## extract html lines with species names and id
 indexphp<-amp.page[grep("index.*id=",amp.page)]
 indexphp[1]
+
 ## create df with species id in ukrbin and scientific name
 id <- str_match(indexphp, 'id=(.*?)"><i>')
 id[,2]
 names<-str_match(indexphp, '<i>(.*?)</i>')
 names[,2]
 df.species<-data.frame(ID=id[,2],names=names[,2])
+## use the species ID to fetch URL of species of interest
 df.occurrences<-data.frame(species=NA,lat=NA,lon=NA)
 for (i in 1:nrow(df.species)) {
   url<-paste0("http://www.ukrbin.com/index.php?id=",df.species$ID[i],"&action=distribution")
   distr.url<-readLines(url)
-  coords<-distr.url[grep('<td class=\"catcolumn5\" valign=\"top\".*align',distr.url)+1]
+  coords<-distr.url[grep('<td class=\"catcolumn5\" valign=\"top\".*align',distr.url)+1] #string to extract species coordinates
   coords<-gsub("\t","",coords)
   coords<-as.numeric(coords)
   coords<-coords[which(coords>1)]
