b81edbb2117573dfb7c7c7ba4131bd43bf37db85
teuzindahuz <m.lattuada87@gmail.com>
2021-02-05 16:03:11 +0100

    Add files via upload

---
+++ moveFiles.R
@@ -0,0 +1,12 @@
+install.packages("stringi")
+install.packages("filesstrings")
+
+library(stringr)
+library(stringi)
+library(filesstrings)
+
+# list all CSV files recursively through each sub-folder
+pdf<-list.files("C:/Users/ASUS/Downloads/here",pattern = ".pdf", full.names=T,recursive = TRUE)
+
+
+for (i in 1:length(pdf)) {file.move(pdf[i], "C:/Users/ASUS/Downloads/here")}
